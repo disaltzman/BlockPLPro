df92dd3dca13f3b75f6759bbf80ea251ef7fda27
David Saltzman <David_Saltzman@d66h194.public.uconn.edu>
2018-04-13 16:04:53 -0400

    initial version

---
+++ production_analysis.R
@@ -0,0 +1,44 @@
+rm(list = ls(all = TRUE))
+
+library(lme4)
+library(ggplot2)
+library(plyr)
+library(R.utils)
+library(nlme)
+library(car)
+library(phia)
+library(multcomp)
+
+theme_set(theme_bw())
+
+data <- data <- as.data.frame(read.delim(file='blockPLPRo_acoustic_analysis.txt', sep='\t'))
+
+# create distribution figures
+# mean
+ggplot(data, aes(x=data$mean,fill=data$cond))+
+    geom_density(alpha=.5,position="identity") +
+    facet_wrap(~subject)
+    
+# mean
+cdat <- ddply(data, "cond2", summarise, rating.mean=mean(mean))
+
+ggplot(data, aes(x=data$mean,fill=data$cond2))+
+  geom_density(alpha=.5,position="identity") + 
+  geom_vline(data=cdat, aes(xintercept=rating.mean,  colour=cond2),
+             linetype="dashed", size=1)
+
+# sd
+ggplot(data, aes(x=data$sd,fill=data$cond2))+
+  geom_density(alpha=.5,position="identity")
+
+# kurtosis
+ggplot(data, aes(x=data$kurtosis,fill=data$cond2))+
+  geom_density(alpha=.5,position="identity")
+
+# dur
+ggplot(data, aes(x=data$dur,fill=data$cond2))+
+  geom_density(alpha=.5,position="identity")
+
+# skew
+ggplot(data, aes(x=data$skew,fill=data$cond2))+
+  geom_density(alpha=.5,position="identity")
