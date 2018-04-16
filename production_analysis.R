5e66dcd4de5734c6aafbcece2c8b77d66e802a52
David Saltzman <disaltzman@gmail.com>
2018-04-16 15:31:23 -0400

    added t-test for each subject's centroid frequency

--- production_analysis.R
+++ production_analysis.R
@@ -2,13 +2,14 @@ rm(list = ls(all = TRUE))
 
 library(lme4)
 library(ggplot2)
+library(tidyr)
 library(plyr)
 library(dplyr)
 library(R.utils)
 
 theme_set(theme_bw())
 
-data <- data <- as.data.frame(read.delim(file='blockPLPRo_acoustic_analysis.txt', sep='\t'))
+data <- as.data.frame(read.delim(file='blockPLPRo_acoustic_analysis.txt', sep='\t'))
 
 # create distribution figures
 # individual subject mean
@@ -42,6 +43,19 @@ ggplot(data, aes(x=data$skew,fill=data$cond2))+
 ####################################################
 
 # calculate average of four cues across productions for each participant for /s/ and /sh/
-
 subjectaverages <- aggregate(data, by=list(data$subject,data$cond),FUN=mean, na.rm=TRUE, drop=TRUE)
 
+# subset data to remove variables we're not interested in
+sub.exp <- select(data,"subject","cond","mean")
+
+# create loop that performs t-test on means for /s/ and /sh/ by individual subject
+p = 1 
+test.output <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames(test.output) <- paste(c('subject','tstatistic','pvalue'))
+for (i in unique(sub.exp$subject)){
+    ttest <- t.test(mean ~ cond, data = sub.exp[sub.exp$subject == i,])
+    test.output[p,1] <- i
+    test.output[p,2] <- ttest$statistic
+    test.output[p,3] <- ttest$p.value
+    p = p + 1
+    }
